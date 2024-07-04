f33a1dc795db21656ab11741d99e0d3a312b851a
Marcelo Gonçalves <marcelo@objctv.one>
2024-07-04 09:34:31 -0300

    <FEAT>[products page]: creating page products

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import Product from "./pages/Product.jsx";
+
+import "./index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <App>
+        <Home />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/product",
+    element: (
+      <App>
+        <Product />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
